cece851ab387c0020677541d2a0cb574ace3376c
Alpy81 <alperen.ertas@gmx.de>
2023-07-20 11:00:08 +0200

    update commit

--- MongoDB/Tag 1 - 18.07./server.js
+++ MongoDB/Tag 1 - 18.07./server.js
@@ -1,74 +1,100 @@
 import express from "express";
 import "./models/index.js";
 import { Post } from "./models/PostModel.js";
+import { Author } from "./models/AuthorModel.js";
+import multer from "multer";
+import morgan from "morgan";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "de3tbljvy",
+  api_key: "122113981293266",
+  api_secret: "VlNJYAKJgZVMucgrJwe0IsLT9LU",
+});
 
 const app = express();
 const PORT = 3001;
+const upload = multer({ storage: multer.memoryStorage() });
 
-// express.json() ist eine eingebaute Middleware-Funktion in Express.js.
-// Sie analysiert den Anfragekörper, wenn er im JSON-Format vorliegt, und setzt das req.body-Objekt auf die geparsten JSON-Daten.
-// Diese Middleware ist erforderlich, um JSON-Daten zu verarbeiten, die von einem Client an den Server gesendet werden, z. B. bei POST-Anfragen mit JSON-Payload.
-// Durch das Hinzufügen dieser Middleware wird Express in der Lage sein, den Anfragekörper zu analysieren und die Daten im JSON-Format für weitere Verarbeitung verfügbar zu machen.
 app.use(express.json());
-
-// const addPost = async (post) => {
-//     const newPost = new Post(post)
-//     console.log(newPost)
-// newPost
-//     const response = await newPost.save()
-//     console.log(response)
-// }
-
-// // addPost({
-// //     title: "Mein erster Post",
-// //     content: "Willkommen auf dem Blog",
-// //     author: "Marius",
-// // })
-// // addPost({
-// //     title: "Mein erster Post",
-// //     content: "Willkommen auf dem Blog",
-// //     author: "Farid",
-// // })
-// // addPost({
-// //     title: "Mein erster Post",
-// //     content: "Willkommen auf dem Blog",
-// //     author: "Olli",
-// // })
-
-// const findPost = async (search) => {
-//     const posts = await Post.find({ author: search })
-//     console.log(posts)
-// }
-// findPost("Olliasd")
-
-// GET-Anfrage an "/api/posts":
-
-// Diese Route handhabt GET-Anfragen an "/api/posts".
-// Die Funktion wird asynchron ausgeführt, um auf die Ergebnisse der Datenbankabfrage zu warten.
-// Post.find() wird verwendet, um alle vorhandenen Beiträge aus der Datenbank abzurufen.
-// Die erhaltenen Daten werden als JSON an den Client zurückgegeben.
+app.use(morgan("dev"));
 
 app.get("/api/posts", async (req, res) => {
   const data = await Post.find();
   res.json(data);
 });
 
-// POST-Anfrage an "/api/addPost":
+app.post("/api/addPost", upload.single("image"), async (req, res) => {
+  try {
+    const author = await Author.findById(req.body.author);
 
-// Diese Route handhabt POST-Anfragen an "/api/addPost".
-// Die Funktion wird asynchron ausgeführt, um auf die Datenbankoperationen zu warten..
-// Post.create(req.body) erstellt einen neuen Datenbankeintrag mit den im Anfragekörper enthaltenen Daten.
-// Die Antwort der Datenbankoperation wird als JSON an den Client zurückgegeben.
+    if (author === null) {
+      return res.send("Author is invalid");
+    }
+    cloudinary.uploader
+      .upload_stream(
+        { resource_type: "image", folder: "MyBlog" },
+        async (err, result) => {
+          const response = await Post.create({
+            ...req.body,
+            image: { url: result.secure_url, imageId: result.public_id },
+          });
+          res.json(response);
+        }
+      )
+      .end(req.file.buffer);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("there was an error");
+  }
+});
 
-app.post("/api/addPost", async (req, res) => {
-  const response = await Post.create(req.body);
-  res.json(response);
+app.put("/api/editPost/:id", async (req, res) => {
+  const edits = req.body;
+  const postId = req.params.id;
 
-  //alternative Ansätze zur Erstellung eines neuen Beitrags zeigen. In diesem Fall wird ein neues Post-Objekt erstellt, der Autor wird festgelegt und der Beitrag wird in der Datenbank gespeichert.
-  // const newPost = new Post(post)
-  // console.log(newPost)
-  // newPost.author ="***"
-  // const response2 = await newPost.save()
+  try {
+    const dbRes = await Post.findByIdAndUpdate(postId, edits, { new: true });
+    res.json(dbRes);
+  } catch (err) {
+    console.log(err);
+    res.send("there was an error");
+  }
+});
+
+app.delete("/api/deletePost/:id", async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const dbRes = await Post.findByIdAndDelete(postId);
+    cloudinary.uploader.destroy(dbRes.image?.imageId, (err) =>
+      console.log(err)
+    );
+    res.send("post has been deleted");
+  } catch (err) {
+    console.log(err);
+    res.send("there was an error");
+  }
+});
+
+app.post("/api/newAuthor", async (req, res) => {
+  try {
+    const newAuthor = await Author.create(req.body);
+    res.json(newAuthor);
+  } catch (err) {
+    console.log(err);
+    res.send("there was an error");
+  }
+});
+
+app.get("/api/getPostsByAuthor/:authorId", async (req, res) => {
+  try {
+    const authorId = req.params.authorId;
+    const posts = await Post.find({ author: authorId });
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.send("there was an error");
+  }
 });
 
 app.listen(PORT, () => console.log("Der Server läuft", PORT));
